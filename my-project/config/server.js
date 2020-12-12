66470ad703f64eb41cc76ba6649a575f6caba24f
Exinord Erold <erold.exi@gmail.com>
2020-12-12 15:08:53 -0500

    first strapi commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd5b2973cfae409036c7cfeed850bf402'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5aa0b77b-6e97-4d59-ac85-9d6200a46952'
+};
